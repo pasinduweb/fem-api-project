122abc94d674a4c1b420b319fe254ba06e4931f9
Pasindu Manaranjana <algopasindu@gmail.com>
2025-09-02 23:17:19 +0530

    Add API route handlers

---
+++ internal/api/workout_handler.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type WorkoutHandler struct{}
+
+func NewWorkoutHandler() *WorkoutHandler {
+	return &WorkoutHandler{}
+}
+
+func (wh *WorkoutHandler) HandleGetWorkoutById(w http.ResponseWriter, r *http.Request) {
+	paramsWorkoutId := chi.URLParam(r, "id")
+	if paramsWorkoutId == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	workoutId, err := strconv.ParseInt(paramsWorkoutId, 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Workout ID is %d\n", workoutId)
+}
+
+func (wh *WorkoutHandler) HandleCreateWorkout(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Workout created\n")
+}
